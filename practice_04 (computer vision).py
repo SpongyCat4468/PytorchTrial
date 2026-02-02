b27e08ac867d7822284773b293977364036fcd66
SpongyCat4468 <spongycat4468@gmail.com>
2026-02-02 18:40:25 +0800

    e

--- practice_04 (computer vision).py
+++ practice_04 (computer vision).py
@@ -1,11 +1,12 @@
 import torch.nn as nn
 import torch
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader 
 
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-
+# Preparing data
 train_data = datasets.FashionMNIST(root="data", train=True, download=True, transform=ToTensor(), target_transform=None)
 test_data = datasets.FashionMNIST(root="data", train=False, download=True, transform=ToTensor(), target_transform=None)
 '''
@@ -28,9 +29,9 @@ plt.imshow(image.squeeze(), cmap="gray")
 plt.show()
 '''
 
+'''
 fig = plt.figure(figsize=(9, 9))
 rows, cols = 4, 4
-class_items = {value: key for key, value in train_data.class_to_idx.items()}
 for i in range(1, rows*cols+1):
     random_idx = torch.randint(0, len(train_data), size=[1]).item()
     img, label = train_data[random_idx]
@@ -39,3 +40,24 @@ for i in range(1, rows*cols+1):
     plt.title(class_items[label])
     plt.axis(False)
 plt.show()
+'''
+
+class_items = {value: key for key, value in train_data.class_to_idx.items()}
+# Preparing dataloader (data loader turns our data into a python iterable) -> breaking data into small batches so memory can load it in
+BATCH_SIZE = 32
+train_dataloader = DataLoader(train_data, BATCH_SIZE, shuffle=True)
+test_dataloader = DataLoader(test_data, BATCH_SIZE, shuffle=False) # dont shuffle -> easier to evaluate the model
+
+train_features_batch, train_label_batch = next(iter(train_dataloader))
+
+'''
+torch.manual_seed(67)
+random_idx = torch.randint(0, len(train_features_batch), size=[1]).item()
+img, label = train_features_batch[random_idx], train_label_batch[random_idx].item()
+plt.imshow(img.squeeze(), cmap="gray")
+plt.title(class_items[label])
+plt.axis(False)
+plt.show()
+'''
+ 
+ 
