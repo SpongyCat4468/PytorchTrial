cca2071b44e13a16d0d7923c477673f7e714a827
SpongyCat4468 <spongycat4468@gmail.com>
2026-02-04 15:06:50 +0800

    dfghjkl

--- practice_04 (computer vision).py
+++ practice_04 (computer vision).py
@@ -12,7 +12,7 @@ from helper_functions import accuracy_fn
 from timeit import default_timer as timer
 
 # Preparing data
-device = "cuda" if torch.cuda.is_available else "cpu"
+device = "cpu" if torch.cuda.is_available else "cpu"
 train_data = datasets.FashionMNIST(root="data", train=True, download=True, transform=ToTensor(), target_transform=None)
 test_data = datasets.FashionMNIST(root="data", train=False, download=True, transform=ToTensor(), target_transform=None)
 '''
@@ -75,8 +75,9 @@ class ComputerVisonModel(nn.Module):
             nn.Flatten(),
             nn.Linear(input, hidden),
             nn.ReLU(),
-            nn.Linear(hidden, output),
-            nn.ReLU()
+            nn.Linear(hidden, hidden),
+            nn.ReLU(),
+            nn.Linear(hidden, output)
         )
 
     def forward(self, x):
@@ -121,10 +122,23 @@ for epoch in tqdm(range(epochs)):
             test_pred = model(X_test.to(device))
             test_loss += loss_fn(test_pred, y_test.to(device))
             test_acc += accuracy_fn(y_test.to(device), test_pred.argmax(dim=1))
+            
         # Calculate test loss average per batch
         test_loss /= len(test_dataloader)
         test_acc /= len(test_dataloader)
-    print(f"\nTrain Loss: {train_loss:.4f} | Test Loss: {test_loss:.4f} | Test Acc: {test_acc:.2f}%")
+    if epoch == 0:
+        print(f"\nTrain Loss: {train_loss:.4f} | Test Loss: {test_loss:.4f} | Test Acc: {test_acc:.2f}%")
+    else:
+        better_train_loss = True if (train_loss.item() < prev_train_loss) else False
+        better_test_loss = True if (test_loss.item() < prev_test_loss) else False
+        better_acc = True if test_acc > prev_acc else False
+        pointer_train_loss = "↑" if better_train_loss else "↓"
+        pointer_test_loss = "↑" if better_test_loss else "↓"
+        pointer_acc = "↑" if better_acc else "↓"
+        print((train_loss.item() < prev_train_loss), (test_loss.item() < prev_test_loss), better_train_loss, better_test_loss)
+        print(f"\nTrain Loss: {train_loss:.4f} {pointer_train_loss}| Test Loss: {test_loss:.4f} {pointer_test_loss}| Test Acc: {test_acc:.2f}% {pointer_acc}")
+        
+    prev_train_loss, prev_test_loss, prev_acc = train_loss.item(), test_loss.item(), test_acc
 train_time_end = timer()
 total_train_time = print_train_time(train_time_start, train_time_end)
 
@@ -159,3 +173,41 @@ if (input("save?").lower() == "y"):
 
 
 # 5 epochs: cpu time -> 33.463 seconds | gpu time -> 39.872 seconds
+
+# Functionalize training loop
+def train_step(model: nn.Module, train_dataloader: DataLoader, loss_fn: nn.Module, optimizer: torch.optim.Optimizer, accuracy_fn, device: torch.device = device):
+    model.train()
+    train_loss, train_acc = 0, 0
+    for batch, (X, y) in enumerate(train_dataloader): # X -> image, y -> label
+        X, y = X.to(device), y.to(device)
+        y_pred = model(X)
+
+        loss = loss_fn(y_pred)
+        train_loss += loss
+        train_acc += accuracy_fn(y, y_pred.argmax(dim=1))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 400 == 0:
+            print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples.")
+    train_loss /= len(train_dataloader)
+    train_acc /= len(train_dataloader)
+    print(f"Train Loss: {train_loss:.4f} | Train Accuracy: {train_acc:.2f}%")
+
+# Functionalizing testing loop
+def test_step(model: nn.Module, test_dataloader: DataLoader, loss_fn: nn.Module, optimizer: torch.optim.Optimizer, accuracy_fn, device: torch.device = device):
+    test_loss, test_acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X_test, y_test in test_dataloader:
+            # test_pred in format of logits
+            test_pred = model(X_test.to(device))
+            test_loss += loss_fn(test_pred, y_test.to(device))
+            test_acc += accuracy_fn(y_test.to(device), test_pred.argmax(dim=1))
+            
+        # Calculate test loss average per batch
+        test_loss /= len(test_dataloader)
+        test_acc /= len(test_dataloader)
+    print(f"Test Loss: {test_loss:.4f} | Test Accuracy: {test_acc:.2f}%")
